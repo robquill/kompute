3cee516b5cce222741a030de5d84ceac539dd466
Robert Quill <robert.quill@imgtec.com>
2025-10-09 12:22:02 +0000

    Use uint8 textures instead of float
    HW may not be able to sample float textures.
    Also tidy up the code by using auto for some complex templated types.

--- examples/image_copy/src/main.cpp
+++ examples/image_copy/src/main.cpp
@@ -1,5 +1,8 @@
+#include <cstdint> // for uint8_t
+#include <cstring> // for memcpy
 #include <iostream>
 #include <memory>
+#include <sys/types.h>
 #include <vector>
 
 #include <kompute/Kompute.hpp>
@@ -36,43 +39,36 @@ main(int argc, char** argv)
     }
 
     size_t imgSize = width * height * 4;
-    std::vector<float> imgF32(imgSize);
-    for (size_t i = 0; i < imgSize; ++i) {
-        imgF32[i] = imgData[i] / 255.0f;
-    }
+    std::vector<uint8_t> imgRGBA8(imgSize);
+    memcpy(imgRGBA8.data(), imgData, imgSize * sizeof(uint8_t));
 
-    std::vector<float> output(imgSize, 0.0f);
+    std::vector<uint8_t> output(imgSize, 0);
 
     stbi_image_free(imgData);
 
-    std::shared_ptr<kp::ImageT<float>> imageIn =
-      mgr.image(imgF32, width, height, 4);
-    std::shared_ptr<kp::ImageT<float>> imageOut =
-      mgr.image(output, width, height, 4);
+    auto imageIn = mgr.imageT<uint8_t>(imgRGBA8, width, height, 4);
+    auto imageOut = mgr.imageT<uint8_t>(output, width, height, 4);
 
-    std::shared_ptr<kp::TextureT<float>> textureIn =
-      mgr.texture(imgF32, width, height, 4);
-    std::shared_ptr<kp::ImageT<float>> textureOut =
-      mgr.image(output, width, height, 4);
+    auto textureIn = mgr.textureT<uint8_t>(imgRGBA8, width, height, 4);
+    auto textureOut = mgr.imageT<uint8_t>(output, width, height, 4);
 
-    std::shared_ptr<kp::TextureT<float>> textureBilinearIn =
-      mgr.texture(imgF32, width, height, 4, vk::Filter::eLinear);
-    std::shared_ptr<kp::ImageT<float>> textureBilinearOut =
-      mgr.image(output, width, height, 4);
+    auto textureBlurIn =
+      mgr.textureT<uint8_t>(imgRGBA8, width, height, 4, vk::Filter::eLinear);
+    auto textureBlurOut = mgr.imageT<uint8_t>(output, width, height, 4);
 
-    const std::vector<uint32_t> image_shader = std::vector<uint32_t>(
+    const auto image_shader = std::vector<uint32_t>(
       shader::IMAGE_COPY_COMP_SPV.begin(), shader::IMAGE_COPY_COMP_SPV.end());
 
-    std::shared_ptr<kp::Algorithm> image_copy = mgr.algorithm(
+    auto image_copy = mgr.algorithm(
       { imageIn, imageOut },
       image_shader,
       { static_cast<unsigned int>(width), static_cast<unsigned int>(height) });
 
-    const std::vector<uint32_t> texture_shader =
+    const auto texture_shader =
       std::vector<uint32_t>(shader::TEXTURE_COPY_COMP_SPV.begin(),
                             shader::TEXTURE_COPY_COMP_SPV.end());
 
-    std::shared_ptr<kp::Algorithm> texture_copy = mgr.algorithm(
+    auto texture_copy = mgr.algorithm(
       { textureIn, textureOut },
       texture_shader,
       { static_cast<unsigned int>(width), static_cast<unsigned int>(height) });
@@ -80,8 +76,8 @@ main(int argc, char** argv)
     const std::vector<uint32_t> texture_blur_shader =
       std::vector<uint32_t>(shader::TEXTURE_BLUR_COMP_SPV.begin(),
                             shader::TEXTURE_BLUR_COMP_SPV.end());
-    std::shared_ptr<kp::Algorithm> texture_blur = mgr.algorithm(
-      { textureIn, textureBilinearOut },
+    auto texture_blur = mgr.algorithm(
+      { textureIn, textureBlurOut },
       texture_blur_shader,
       { static_cast<unsigned int>(width), static_cast<unsigned int>(height) });
 
@@ -99,22 +95,16 @@ main(int argc, char** argv)
       ->record<kp::OpSyncLocal>({ textureIn, textureOut })
       ->eval();
 
-    // Texture copy using sampled texture bilinear filtering
+    // Texture blur using bilinear filtering
     mgr.sequence()
-      ->record<kp::OpSyncDevice>({ textureIn, textureBilinearOut })
+      ->record<kp::OpSyncDevice>({ textureIn, textureBlurOut })
       ->record<kp::OpAlgoDispatch>(texture_blur)
-      ->record<kp::OpSyncLocal>({ textureIn, textureBilinearOut })
+      ->record<kp::OpSyncLocal>({ textureIn, textureBlurOut })
       ->eval();
 
     // Save tensors to JPEG
     std::vector<unsigned char> outImg(imgSize);
-    size_t i = 0;
-    for (const float& elem : imageOut->vector()) {
-        outImg[i] = static_cast<unsigned char>(
-          std::max(0.0f, std::min(1.0f, elem)) * 255.0f);
-        i++;
-    }
-
+    memcpy(outImg.data(), imageOut->vector().data(), imgSize * sizeof(uint8_t));
     if (!stbi_write_jpg(
           "output_image.jpg", width, height, 4, outImg.data(), 100)) {
         std::cerr << "Failed to write output_image.jpg" << std::endl;
@@ -123,13 +113,8 @@ main(int argc, char** argv)
 
     std::cout << "Saved output_image.jpg" << std::endl;
 
-    i = 0;
-    for (const float& elem : textureOut->vector()) {
-        outImg[i] = static_cast<unsigned char>(
-          std::max(0.0f, std::min(1.0f, elem)) * 255.0f);
-        i++;
-    }
-
+    memcpy(
+      outImg.data(), textureOut->vector().data(), imgSize * sizeof(uint8_t));
     if (!stbi_write_jpg(
           "output_texture.jpg", width, height, 4, outImg.data(), 100)) {
         std::cerr << "Failed to write output_texture.jpg" << std::endl;
@@ -138,24 +123,20 @@ main(int argc, char** argv)
 
     std::cout << "Saved output_texture.jpg" << std::endl;
 
-    i = 0;
-    for (const float& elem : textureBilinearOut->vector()) {
-        outImg[i] = static_cast<unsigned char>(
-          std::max(0.0f, std::min(1.0f, elem)) * 255.0f);
-        i++;
-    }
-
-    if (!stbi_write_jpg("output_texture_bilinear.jpg",
+    memcpy(outImg.data(),
+           textureBlurOut->vector().data(),
+           imgSize * sizeof(uint8_t));
+    if (!stbi_write_jpg("output_texture_blur.jpg",
                         width,
                         height,
                         4,
                         outImg.data(),
                         100)) {
-        std::cerr << "Failed to write output_texture_bilinear.jpg" << std::endl;
+        std::cerr << "Failed to write output_texture_blur.jpg" << std::endl;
         return 1;
     }
 
-    std::cout << "Saved output_texture_bilinear.jpg" << std::endl;
+    std::cout << "Saved output_texture_blur.jpg" << std::endl;
 
     return 0;
 }
